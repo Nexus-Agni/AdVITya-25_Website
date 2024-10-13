039b898bc09ddf38abe85413322c3751ef69856d
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-10-13 16:17:34 +0530

    Added Club Events Section

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { Leadership } from "../components/Leadership";
+import { AdvityaDescription } from "../components/AdVITyaDescription";
+import { ClubEvents } from "../components/ClubEvents";
+import { Sponsors } from "../components/Sponsors";
+
+function Home() {
+  return (
+    <div>
+      <div className="bg-black w-full">
+        Advitya Website
+        <Leadership />
+        <AdvityaDescription classname="bg-black" />
+        <ClubEvents />
+        <Sponsors />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
